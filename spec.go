67785e0d82be61f8ac6fdf78b9fd234d0920ca7d
Tim Schindler <xh3b4sd@gmail.com>
2017-05-31 18:48:09 +0200

    added namespace property to custom object spec

--- spec.go
+++ spec.go
@@ -6,6 +6,10 @@ import (
 )
 
 type Spec struct {
-	Host    host.Host       `json:"host" yaml:"host"`
-	Network network.Network `json:"network" yaml:"network"`
+	Host host.Host `json:"host" yaml:"host"`
+	// Namespace is the namespace of the guest cluster to watch for. E.g. Flannel
+	// bridges should not be removed until there is any workload running within
+	// the guest cluster's namespace.
+	Namespace string          `json:"namespace" yaml:"namespace"`
+	Network   network.Network `json:"network" yaml:"network"`
 }
