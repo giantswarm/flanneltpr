9cff63aacac6d63ae3ebd5e8afe6d024901dbca9
Paweł Kopiczko <kopiczko@gmail.com>
2017-07-07 19:10:10 +0200

    add DNS & NTP config

--- bridge/config.go
+++ bridge/config.go
@@ -8,4 +8,9 @@ type Config struct {
 	// PrivateNetwork is the host's private network to block against, e.g.
 	// 10.0.4.0/24.
 	PrivateNetwork string `json:"privateNetwork" yaml:"privateNetwork"`
+
+	// DNS holds DNS configuration for the bridge.
+	DNS DNS `json:"dns" yaml:"dns"`
+	// NTP holds NTP configuration for the bridge.
+	NTP NTP `json:"ntp" yaml:"ntp"`
 }
