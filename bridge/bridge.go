e606a5f558de318c3b56fd4364165f2ce17ddc43
Paweł Kopiczko <kopiczko@gmail.com>
2017-07-12 14:37:59 +0200

    create separate Docker struct for each package using it

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -1,14 +1,10 @@
 package bridge
 
-import (
-	"github.com/giantswarm/flanneltpr/docker"
-)
-
 // Bridge holds information used for the execution of the Docker entrypoint of
 // https://github.com/giantswarm/k8s-network-bridge.
 type Bridge struct {
 	// Config contains network bridge configuration.
 	Config Config `json:"config" yaml:"config"`
 	// Docker describes the docker image running k8s-network-bridge.
-	Docker docker.Docker `json:"docker" yaml:"docker"`
+	Docker Docker `json:"docker" yaml:"docker"`
 }
--- flannel/flannel.go
+++ flannel/flannel.go
@@ -1,9 +1,5 @@
 package flannel
 
-import (
-	"github.com/giantswarm/flanneltpr/docker"
-)
-
 // Flannel holds the configuration to run falnneld and create etcd VNI
 // configuration for it.
 // https://github.com/coreos/flannel/blob/master/Documentation/configuration.md
@@ -11,5 +7,5 @@ type Flannel struct {
 	// Config contains flannel configuration.
 	Config Config `json:"config" yaml:"config"`
 	// Docker describes the docker image running flanneld.
-	Docker docker.Docker `json:"docker" yaml:"docker"`
+	Docker Docker `json:"docker" yaml:"docker"`
 }
